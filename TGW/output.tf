72076c1937e089db91757c9ce27cc11643f3f89a
Serafino Stoimenov <40893656+serafino16@users.noreply.github.com>
2025-05-02 09:33:35 +0300

    Update output.tf

--- TGW/output.tf
+++ TGW/output.tf
@@ -1,3 +1,16 @@
 output "transit_gateway_id" {
   value = aws_ec2_transit_gateway.this.id
 }
+
+output "route_table_id" {
+  value = try(aws_ec2_transit_gateway_route_table.this[0].id, null)
+}
+
+output "vpc_attachment_ids" {
+  value = { for k, v in aws_ec2_transit_gateway_vpc_attachment.this : k => v.id }
+}
+
+output "peering_attachment_ids" {
+  value = { for k, v in aws_ec2_transit_gateway_peering_attachment.this : k => v.id }
+}
+

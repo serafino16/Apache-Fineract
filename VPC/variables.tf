4720a38832e2533f87c23d363f742b902105e42a
Serafino Stoimenov <40893656+serafino16@users.noreply.github.com>
2025-05-09 11:25:26 +0300

    Update variables.tf

--- VPC/variables.tf
+++ VPC/variables.tf
@@ -1,30 +1,37 @@
-variable "vpc_name" {
-  description = "Name of the VPC."
-  type        = string
-}
-
 variable "cidr_block" {
-  description = "CIDR block for the VPC."
+  description = "The CIDR block for the VPC"
   type        = string
 }
 
-variable "azs" {
-  description = "List of availability zones to create subnets in."
-  type        = list(string)
+variable "vpc_name" {
+  description = "Name tag for the VPC and associated resources"
+  type        = string
+}
+
+variable "tags" {
+  description = "A map of tags to apply to resources"
+  type        = map(string)
+  default     = {}
 }
 
 variable "public_subnets" {
-  description = "List of public subnet CIDRs."
+  description = "List of public subnet CIDR blocks"
   type        = list(string)
 }
 
 variable "private_subnets" {
-  description = "List of private subnet CIDRs."
+  description = "List of private subnet CIDR blocks"
   type        = list(string)
 }
 
-variable "tags" {
-  description = "Tags to apply to all resources."
-  type        = map(string)
-  default     = {}
+variable "azs" {
+  description = "List of availability zones to use for the subnets"
+  type        = list(string)
 }
+
+variable "enable_nat_gateway" {
+  description = "Whether to provision a NAT Gateway (true/false)"
+  type        = bool
+  default     = true
+}
+

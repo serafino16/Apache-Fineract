3bfa3f0d3d141e5ac84676e9d2d5a2c6ecf76c06
Serafino Stoimenov <40893656+serafino16@users.noreply.github.com>
2025-05-09 11:24:39 +0300

    Update vpc.tf

--- VPC/vpc.tf
+++ VPC/vpc.tf
@@ -8,13 +8,33 @@ resource "aws_vpc" "this" {
   })
 }
 
+
+resource "aws_internet_gateway" "this" {
+  vpc_id = aws_vpc.this.id
+
+  tags = merge(var.tags, {
+    Name = "${var.vpc_name}-igw"
+  })
+}
+
+
+resource "aws_eip" "nat" {
+  count = var.enable_nat_gateway ? 1 : 0
+
+  vpc = true
+
+  tags = merge(var.tags, {
+    Name = "${var.vpc_name}-nat-eip"
+  })
+}
+
+
 resource "aws_subnet" "public" {
   count = length(var.public_subnets)
 
-  vpc_id     = aws_vpc.this.id
-  cidr_block = element(var.public_subnets, count.index)
-  availability_zone = element(var.azs, count.index)
-
+  vpc_id                  = aws_vpc.this.id
+  cidr_block              = var.public_subnets[count.index]
+  availability_zone       = var.azs[count.index]
   map_public_ip_on_launch = true
 
   tags = merge(var.tags, {
@@ -23,15 +43,79 @@ resource "aws_subnet" "public" {
   })
 }
 
+
 resource "aws_subnet" "private" {
   count = length(var.private_subnets)
 
-  vpc_id     = aws_vpc.this.id
-  cidr_block = element(var.private_subnets, count.index)
-  availability_zone = element(var.azs, count.index)
+  vpc_id            = aws_vpc.this.id
+  cidr_block        = var.private_subnets[count.index]
+  availability_zone = var.azs[count.index]
 
   tags = merge(var.tags, {
     Name = "${var.vpc_name}-private-${count.index}"
     Type = "Private Subnet"
   })
 }
+
+
+resource "aws_nat_gateway" "this" {
+  count = var.enable_nat_gateway ? 1 : 0
+
+  allocation_id = aws_eip.nat[0].id
+  subnet_id     = aws_subnet.public[0].id
+
+  tags = merge(var.tags, {
+    Name = "${var.vpc_name}-nat-gateway"
+  })
+}
+
+
+resource "aws_route_table" "public" {
+  vpc_id = aws_vpc.this.id
+
+  tags = merge(var.tags, {
+    Name = "${var.vpc_name}-public-rt"
+  })
+}
+
+
+resource "aws_route" "public_internet_access" {
+  route_table_id         = aws_route_table.public.id
+  destination_cidr_block = "0.0.0.0/0"
+  gateway_id             = aws_internet_gateway.this.id
+}
+
+
+resource "aws_route_table_association" "public" {
+  count          = length(aws_subnet.public)
+  subnet_id      = aws_subnet.public[count.index].id
+  route_table_id = aws_route_table.public.id
+}
+
+
+resource "aws_route_table" "private" {
+  count  = length(var.private_subnets)
+
+  vpc_id = aws_vpc.this.id
+
+  tags = merge(var.tags, {
+    Name = "${var.vpc_name}-private-rt-${count.index}"
+  })
+}
+
+
+resource "aws_route" "private_nat_gateway" {
+  count = var.enable_nat_gateway ? length(var.private_subnets) : 0
+
+  route_table_id         = aws_route_table.private[count.index].id
+  destination_cidr_block = "0.0.0.0/0"
+  nat_gateway_id         = aws_nat_gateway.this[0].id
+}
+
+
+resource "aws_route_table_association" "private" {
+  count          = length(var.private_subnets)
+  subnet_id      = aws_subnet.private[count.index].id
+  route_table_id = aws_route_table.private[count.index].id
+}
+

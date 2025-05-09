cf541574699724a288107b47cce1840bb08c9f54
Serafino Stoimenov <40893656+serafino16@users.noreply.github.com>
2025-05-09 11:25:43 +0300

    Update outputs.tf

--- VPC/outputs.tf
+++ VPC/outputs.tf
@@ -1,11 +1,39 @@
 output "vpc_id" {
-  value = aws_vpc.this.id
+  description = "The ID of the VPC"
+  value       = aws_vpc.this.id
 }
 
-output "public_subnets" {
-  value = aws_subnet.public[*].id
+output "public_subnet_ids" {
+  description = "List of IDs of public subnets"
+  value       = aws_subnet.public[*].id
 }
 
-output "private_subnets" {
-  value = aws_subnet.private[*].id
+output "private_subnet_ids" {
+  description = "List of IDs of private subnets"
+  value       = aws_subnet.private[*].id
+}
+
+output "internet_gateway_id" {
+  description = "The ID of the Internet Gateway"
+  value       = aws_internet_gateway.this.id
+}
+
+output "nat_gateway_id" {
+  description = "The ID of the NAT Gateway"
+  value       = var.enable_nat_gateway ? aws_nat_gateway.this[0].id : null
+}
+
+output "nat_eip_id" {
+  description = "The ID of the EIP attached to NAT Gateway"
+  value       = var.enable_nat_gateway ? aws_eip.nat[0].id : null
+}
+
+output "public_route_table_id" {
+  description = "Route table ID for public subnets"
+  value       = aws_route_table.public.id
+}
+
+output "private_route_table_ids" {
+  description = "Route table IDs for private subnets"
+  value       = aws_route_table.private[*].id
 }

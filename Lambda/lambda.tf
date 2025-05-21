1dcfcf5162737cd18562aae21bde2ddea0106034
Serafino Stoimenov <40893656+serafino16@users.noreply.github.com>
2025-05-21 11:44:31 +0300

    Update lambda.tf

--- Lambda/lambda.tf
+++ Lambda/lambda.tf
@@ -1,40 +1,56 @@
-resource "aws_lambda_function" "this" {
-  for_each = toset(var.regions)
-
-  function_name = "loan-creation-lambda-${each.value}"
-
-  role    = var.lambda_roles[each.value]
-  handler = var.lambda_handler
-  runtime = var.lambda_runtime
-
-  filename         = var.lambda_zip_path
-  source_code_hash = filebase64sha256(var.lambda_zip_path)
-
-  environment {
-    variables = {
-      SQS_QUEUE_URL = var.sqs_queues[each.value]
-    }
-  }
-
-  tags = var.tags
-}
-resource "aws_iam_role" "this" {
-  for_each = toset(var.regions)
-
-  name = "lambda-loan-creation-role-${each.value}"
+resource "aws_iam_role" "lambda_exec_role" {
+  name = "${var.lambda_name}-exec-role"
 
   assume_role_policy = jsonencode({
     Version = "2012-10-17"
-    Statement = [
-      {
-        Effect = "Allow"
-        Principal = {
-          Service = "lambda.amazonaws.com"
-        }
-        Action = "sts:AssumeRole"
+    Statement = [{
+      Action    = "sts:AssumeRole"
+      Effect    = "Allow"
+      Principal = {
+        Service = "lambda.amazonaws.com"
       }
-    ]
+    }]
   })
+}
 
-  tags = var.tags
+resource "aws_iam_role_policy_attachment" "lambda_logs" {
+  role       = aws_iam_role.lambda_exec_role.name
+  policy_arn = "arn:aws:iam::aws:policy/service-role/AWSLambdaBasicExecutionRole"
+}
+
+resource "aws_lambda_function" "consumer" {
+  function_name = var.lambda_name
+  role          = aws_iam_role.lambda_exec_role.arn
+  handler       = var.handler
+  runtime       = var.runtime
+  filename      = var.lambda_zip_file
+  memory_size   = var.memory_size
+  timeout       = var.timeout
+
+  environment {
+    variables = var.environment_variables
+  }
+}
+
+resource "aws_lambda_event_source_mapping" "kafka" {
+  event_source_arn  = var.event_source_arn
+  function_name     = aws_lambda_function.consumer.arn
+  starting_position = var.starting_position
+  batch_size        = var.batch_size
+  enabled           = var.enabled
+  kafka_consumer_group_id = var.consumer_group_id
+}
+resource "aws_lambda_function" "producer" {
+  function_name = var.lambda_name
+  role          = aws_iam_role.lambda_exec_role.arn
+  handler       = var.handler
+  runtime       = var.runtime
+  filename      = var.lambda_zip_file
+  memory_size   = var.memory_size
+  timeout       = var.timeout
+
+  environment {
+    variables = var.environment_variables
+  }
+}
 }

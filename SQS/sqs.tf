f839af54bab52a2a4135273c3aa757c8ce711bfd
Serafino Stoimenov <40893656+serafino16@users.noreply.github.com>
2025-05-22 14:11:12 +0300

    Delete SQS directory

--- SQS/sqs.tf
+++
@@ -1,9 +0,0 @@
-resource "aws_sqs_queue" "this" {
-  for_each = toset(var.regions)
-
-  name                        = "user-creation-queue-${each.value}"
-  delay_seconds               = var.delay_seconds
-  message_retention_seconds   = var.message_retention_seconds
-
-  tags = var.tags
-}
